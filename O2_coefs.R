0650854f75f967e5e30534f8b0bf4d235c7fa7fd
JARS3N <JARS3N@users.noreply.github.com>
2017-04-11 15:33:43 -0400

    Delete O2_coefs.R

--- O2_coefs.R
+++
@@ -1,8 +0,0 @@
-  O2_coefs<-function(X){
-    Analyte<-AnalyteIndex(X)
-    coefs<- list(
-      "target"= TargetEmissions(X) %>% .['O2'] %>% as.numeric(),
-      "KSV"=xml_nodes(X,"CorrectedKsv")  %>% xml_text() %>% as.numeric()
-    )
-    coefs
-  }

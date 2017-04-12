14073115f08198ccd37ce57ad158baad4ab69eba
JARS3N <JARS3N@users.noreply.github.com>
2017-04-12 11:20:04 -0400

    Update get_lvls.R

--- R/get_lvls.R
+++ R/get_lvls.R
@@ -1,31 +1,32 @@
-  get_lvls<-function(X){
-    TickTable<-function(X){
-      data.frame(
-        start=xml_nodes(X,'StartTickIndex') %>%
-          xml_text() %>% as.numeric() ,
-        end=xml_nodes(X,'EndTickIndex') %>%
-          xml_text() %>% as.numeric()
-      ) %>% mutate(.,Measure=seq(nrow(.))) %>%
-        rowwise() %>%
-        do(data.frame(Tick=seq(.$start,.$end),Measure=.$Measure))
-    }
-    tick_level<-function(h){
-      xml_nodes(h,'CorrectedEmissionValues') %>%
-      {setNames(
-        lapply(.,{. %>%
-            xml_children %>%
-            xml_text()}),
-        xml_nodes(h,'AnalyteName') %>% xml_text() %>% as.numeric()
-      ) %>%
-          bind_cols()} %>%
-        mutate(.,Well=seq(nrow(.)))
-    }
-    xml_nodes(X,"AnalyteDataSetsByAnalyteName") %>%
-      lapply(testFn) %>%
-      {lapply(seq(.),function(u){mutate(.[[u]],Tick=u)})
-      } %>%
-      bind_rows() %>%
-      rename(.,pH.LVL=pH,O2.LVL=O2) %>%
-      merge(.,TickTable(X),by='Tick')
-
+get_lvls<-function(X){
+  TickTable<-function(X){
+    data.frame(
+      start=xml_nodes(X,'StartTickIndex') %>%
+        xml_text() %>% as.numeric() ,
+      end=xml_nodes(X,'EndTickIndex') %>%
+        xml_text() %>% as.numeric()
+    ) %>% mutate(.,Measure=seq(nrow(.))) %>%
+      rowwise() %>%
+      do(data.frame(Tick=seq(.$start,.$end),Measure=.$Measure))
   }
+  
+  getcounts<-function(x){
+    counts<-xml_nodes(x,'CorrectedEmissionValues') %>% 
+      lapply(.,{. %>%
+          xml_children %>%
+          xml_text() %>% 
+          as.numeric()})
+    analytes<- xml_nodes(x,'AnalyteName') %>% 
+      xml_text() 
+    setNames(counts,analytes) %>% 
+      bind_cols() %>% 
+      mutate(.,Well=seq(nrow(.)))
+  }
+  
+  xml_nodes(X,"AnalyteDataSetsByAnalyteName") %>% 
+  lapply(.,getcounts) 
+  mapply(function(x,y){mutate(x,Tick=y) %>% bind_cols()},B,seq(B),SIMPLIFY = F)  %>% 
+  bind_rows() %>% 
+  inner_join(.,TickTable(X),by='Tick')
+
+}#end

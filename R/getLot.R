3e6fd884c6253f1040d4617e5a3615c6836f2e22
JARS3N <JARS3N@users.noreply.github.com>
2017-04-12 11:49:58 -0400

    Update getLot.R

--- R/getLot.R
+++ R/getLot.R
@@ -1,12 +1,12 @@
   getLot<-function(X){
-    Lot<-paste0(
-      xml_node('Cartridge') %>% xml_node("Type") %>% xml_text(),
-      xml_node('Cartridge') %>% xml_node("Lot") %>% xml_text()
-    )
-    if (length(Lot)==0|Lot==""){
-      flx<-getFile(X)
-      regexp<-"[W|B|C|Q|T]{1}[E|0-9]{1}[0-9]{4}"
-      Lot<- paste0(unlist(regmatches( flx, gregexpr(regexp, flx))))
-    }
-    return(Lot)
+  Lot <- paste0(xml_node(X,"Cartridge") %>% xml_node("Type") %>% 
+                  xml_text(), xml_node(X,"Cartridge") %>% xml_node("Lot") %>% 
+                  xml_text())
+  if (length(Lot) == 0 | Lot == "") {
+    flx <- axml::getFile(X)
+    regexp <- "[W|B|C|Q|T]{1}[E|0-9]{1}[0-9]{4}"
+    Lot <- paste0(unlist(regmatches(flx, gregexpr(regexp, 
+                                                  flx))))
   }
+    return(Lot)
+}

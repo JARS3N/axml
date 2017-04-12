3adfb286586938b9c316cb34a7bea5de527edf65
JARS3N <JARS3N@users.noreply.github.com>
2017-04-12 12:00:44 -0400

    Update newGain.R

--- R/newGain.R
+++ R/newGain.R
@@ -2,5 +2,5 @@
     filter(X$LVL,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
       group_by(.,Well) %>% summarise(.,sorpH=mean(pHlvl)) %>%
       merge(.,X$CAL,by='Well') %>%mutate(.,Target=X$PH_COEF$target) %>%
-      mutate(.,Gain=(Target/pH.CalEmission)*(1/800)*(pH.CalEmission-sorpH))
+      mutate(.,Gain=(Target/pH.CalibrationEmission)*(1/800)*(pH.CalibrationEmission-sorpH))
  }
